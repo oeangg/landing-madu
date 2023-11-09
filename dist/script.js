dd8dcdc2e13513944ee8351e49c520890fdb42c1
oeangg <msubhan33@gmail.com>
2023-11-09 23:19:39 +0700

    menambahkan transition

--- dist/script.js
+++ dist/script.js
@@ -1,13 +1,10 @@
 const hamburger = document.querySelector("#hamburger");
 const navMenu = document.querySelector("#nav-menu");
 
-
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
-
-
 
 window.onscroll = function () {
   const header = document.querySelector("header");
@@ -15,7 +12,11 @@ window.onscroll = function () {
 
   if (window.scrollY > posisiNav) {
     header.classList.add("navbar-fixed");
+    header.classList.add("bg-orange-200");
+    header.classList.remove("bg-transparent");
   } else {
     header.classList.remove("navbar-fixed");
+    header.classList.remove("bg-orange-200");
+    header.classList.add("bg-transparent");
   }
 };
